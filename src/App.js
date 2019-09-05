bf486e246c9c4f3e00da494800d0cbf0d5a861d6
Ramnani <kapil.ramnani@capgemini.com>
2019-09-05 19:13:24 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './App.css'
+import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [{
+      id: uuid.v4(),
+      title: 'Take out the trash',
+      completed: false
+    },{
+      id: uuid.v4(),
+      title: 'Dinner with wife',
+      completed: false
+    },{
+      id: uuid.v4(),
+      title: 'Meeting with boss',
+      completed: false
+    }
+  ]
+  };
+
+  // Toggle Complete
+  markComplete = (id) => {
+   this.setState({ todos: this.state.todos.map(todo => {
+     if(todo.id === id) {
+      todo.completed = !todo.completed
+     }
+     return todo;
+   }) 
+  });   
+  }
+
+  // Delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
+  }
+  
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] })
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <AddTodo addTodo={this.addTodo}/>
+            <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
